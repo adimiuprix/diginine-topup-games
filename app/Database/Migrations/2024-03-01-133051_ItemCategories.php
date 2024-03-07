47582d7122eb8ab5c44235e9b1a872437ab08125
adimiuprix <miuprix@gmail.com>
2024-03-07 08:54:14 +0700

    Gambar seed kosongan

--- app/Database/Migrations/2024-03-01-133051_ItemCategories.php
+++ app/Database/Migrations/2024-03-01-133051_ItemCategories.php
@@ -17,42 +17,52 @@ class ItemCategories extends Migration
             ],
             'id_cats' => [
                 'type'          => 'INT',
-                'constraint'    => 100
+                'constraint'    => 100,
+                'null' => true,
             ],
             'item_name' => [
                 'type'          => 'VARCHAR',
-                'constraint'    => 255
+                'constraint'    => 255,
+                'null' => true,
             ],
             'status' => [
                 'type'       => 'ENUM("enable", "disable")',
                 'default' => 'enable',
+                'null' => true,
             ],
             'slug' => [
                 'type'          => 'VARCHAR',
-                'constraint'    => 255
+                'constraint'    => 255,
+                'null' => true,
             ],
             'image' => [
                 'type'          => 'VARCHAR',
-                'constraint'    => 255
+                'constraint'    => 255,
+                'null' => true,
             ],
             'blog_image' => [
                 'type'          => 'VARCHAR',
-                'constraint'    => 255
+                'constraint'    => 255,
+                'null' => true,
             ],
             'breadcrumb' => [
                 'type'          => 'VARCHAR',
-                'constraint'    => 255
+                'constraint'    => 255,
+                'null' => true,
             ],
             'description' => [
                 'type'          => 'TEXT',
+                'null' => true,
             ],
             'vendor' => [
                 'type'          => 'VARCHAR',
-                'constraint'    => 255
+                'constraint'    => 255,
+                'null' => true,
             ],
             'coloum' => [
                 'type'       => 'ENUM("1", "2")',
                 'default' => '1',
+                'null' => true,
             ],
         ]);
         $this->forge->addKey('id', true);
