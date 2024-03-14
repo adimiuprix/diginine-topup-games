3e6b17ce769b9acfc77fb01287a03e964083191d
adimiuprix <miuprix@gmail.com>
2024-03-14 16:04:34 +0700

    Payment select

--- app/Views/partials/payment-select.php
+++ app/Views/partials/payment-select.php
@@ -1,5 +1,17 @@
 <div data-wow-delay="0s" class="wow fadeInRight product-item traits animated" style="visibility: visible; animation-delay: 0s; animation-name: fadeInRight;">
     <h6><i class="icon-description"></i>Pilih pembayaran</h6>
+
+    <?php if($detailProduct['item_name'] == 'Mobile Legends'): ?>
+    <div class="widget-category-checkbox style-1">
+        <div class="content-wg-category-checkbox">
+            <label>
+                <input type="radio" name="payment" value="7">
+                Manual
+                <span class="btn-checkbox"></span>
+            </label>
+        </div>
+    </div>
+    <?php else: ?>
     <div class="widget-category-checkbox style-1">
         <div class="content-wg-category-checkbox">
             <?php foreach ($methods as $method): ?>
@@ -12,4 +24,6 @@
             <?php endforeach; ?>
         </div>
     </div>
+    <?php endif; ?>
+
 </div>
